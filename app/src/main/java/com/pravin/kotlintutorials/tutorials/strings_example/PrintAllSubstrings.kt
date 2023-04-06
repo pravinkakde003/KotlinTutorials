455d23bbe22ede2a7bcccaf1bf78aeda87d188db
PravinK <pravin.kakde003@gmail.com>
2023-04-06 15:42:30 +0530

    Added some examples

--- app/src/main/java/com/pravin/kotlintutorials/tutorials/strings_example/PrintAllSubstrings.kt
+++ app/src/main/java/com/pravin/kotlintutorials/tutorials/strings_example/PrintAllSubstrings.kt
@@ -8,16 +8,33 @@ fun main() {
 }
 
 // Function to print all substring
-fun printAllSubstring(str: String, n: Int) {
+fun printAllSubstring(inputString: String, n: Int) {
+//    F
+//    Fu
+//    Fun
+//    u
+//    un
+//    n
     for (i in 0 until n) {
         for (j in i + 1..n) {
-            println(str.substring(i, j))
+            println(inputString.substring(i, j))
         }
     }
+    println("---------")
+
+    // other Way
+    // [Fun, Fu, un, F, u, n]
+    val substrings = with(inputString) {
+        indices.asSequence().flatMap { i ->
+            windowedSequence(length - i)
+        }
+    }
+    println(substrings.toList())
 }
 
-fun printSubstringOfGivenSize(str: String, n: Int) {
-    for (i in 0 until str.length - n + 1) {
-        print(str.substring(i, i + n) + " ")
+// Fu un
+fun printSubstringOfGivenSize(inputString: String, n: Int) {
+    for (i in 0 until inputString.length - n + 1) {
+        print(inputString.substring(i, i + n) + " ")
     }
 }
